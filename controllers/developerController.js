43521542e337e6c281b7ab1eaa42ad5b0a6e6143
Goran Jovic <goran1010jovic@gmail.com>
2025-06-05 13:41:33 +0200

    Remove console.logs

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -2,7 +2,6 @@ import getDeveloper from "../models/getDeveloper.js";
 
 export default async function developerController(req, res) {
   const developerName = req.params.developer;
-  console.log(developerName);
   const developerDetails = await getDeveloper(developerName);
 
   res.render("developer", { developerDetails, developerName });
--- controllers/modifyGameController.js
+++ controllers/modifyGameController.js
@@ -2,7 +2,6 @@ import modifyGame from "../models/modifyGame.js";
 
 export default async function modifyGameController(req, res) {
   const { developer, game } = req.params;
-  console.log(developer);
   const { name, genre, price, developer_id } = req.body;
   await modifyGame(name, genre, price, developer_id, game);
   res.redirect(`/${developer}/${name}`);
